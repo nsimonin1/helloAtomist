c8f6c245af9cc25f6d0eadebe930c33bc3803ab6
Ngos Simon Pascal <nsimonin1@yahoo.fr>
2017-12-22 22:16:40 +0000

    Initial commit from Atomist

---
+++ src/main/java/org/simon/pascal/DemoAtomistApplication.java
@@ -0,0 +1,12 @@
+package org.simon.pascal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAtomistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAtomistApplication.class, args);
+	}
+}
